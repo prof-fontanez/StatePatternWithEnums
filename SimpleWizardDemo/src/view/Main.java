e43c09035642eebff2a52486846479e55f67142a
Hector J Fontanez <prof-fontanez@users.noreply.github.com>
2022-04-02 13:05:18 -0500

    Modified main method to take an argument that specifies which should be the initial state and, by extension, the initial panel.

--- SimpleWizardDemo/src/view/Main.java
+++ SimpleWizardDemo/src/view/Main.java
@@ -8,51 +8,61 @@ import java.util.Observer;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-import controller.FirstState;
+import controller.State;
 import model.Context;
 
-public class Main extends JFrame implements Observer
-{
+public class Main extends JFrame implements Observer {
 	private static final long serialVersionUID = 1L;
-	private Context context = new Context(FirstState.INSTANCE);
+	private Context context;
+	private CardsPanel cards;
+	private ButtonPanel buttons;
 
-	private CardsPanel cards = new CardsPanel(context.getCurrentState().getName());
-	private ButtonPanel buttons = new ButtonPanel(context);
-	
-	public Main()
-	{
+	public Main(String initialPanel) {
 		setSize(400, 400);
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		add(cards, BorderLayout.CENTER);
-		add(buttons, BorderLayout.SOUTH);
-		setTitle("Simple Wizard Demo");
-		context.addObserver(this);
+		
+		try {
+			Enum<?> myEnum = getInstance("controller." + initialPanel);
+			context = new Context((State) myEnum);
+			cards = new CardsPanel(initialPanel);
+			add(cards, BorderLayout.CENTER);
+
+			buttons = new ButtonPanel(context);
+			add(buttons, BorderLayout.SOUTH);
+
+			setTitle("Simple Wizard Demo");
+			context.addObserver(this);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static void main(String[] args)
-	{
-		SwingUtilities.invokeLater(new Runnable()
-		{
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			throw new RuntimeException("Need to pass state enum's name as a sigle argument (i.e. FirstState)");
+		}
+		SwingUtilities.invokeLater(new Runnable() {
 			@Override
-			public void run()
-			{
-				createAndShowGUI();
+			public void run() {
+				createAndShowGUI(args[0]);
 			}
 		});
 	}
-	
-	private static void createAndShowGUI()
-	{
-		new Main();
+
+	@SuppressWarnings("unchecked")
+	public static <T extends Enum<T>> T getInstance(String name) throws ClassNotFoundException {
+		return Enum.valueOf((Class<T>) Class.forName(name), "INSTANCE");
+	}
+
+	private static void createAndShowGUI(String initialPanel) {
+		new Main(initialPanel);
 	}
 
 	@Override
-	public void update(Observable o, Object arg)
-	{
-		if(context == o)
-		{
+	public void update(Observable o, Object arg) {
+		if (context == o) {
 			((CardLayout) cards.getLayout()).show(cards, context.getCurrentState().getName());
 		}
 	}
